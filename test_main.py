63a26befb765e07e91f5dffa6935c90421086a9b
Matvey <123477397+Matvey-Doshchechnikov@users.noreply.github.com>
2025-03-21 12:52:24 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import add_numbers
 
 class TestAddition(unittest.TestCase):
     def test_add_numbers(self):
-        self.assertEqual(add_numbers(), 10)
+        self.assertEqual(add_numbers(), 15)
 
 if __name__ == "__main__":
     unittest.main()
